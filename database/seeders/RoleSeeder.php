91f37c99c1812a45e66af18575f4536b33955aaf
wayhyou <ekow60006@gmail.com>
2025-08-10 23:06:29 +0700

    feat: add roles and permissions setup in RoleSeeder
    - Create roles: Admin, HRD, Atasan, Karyawan
    - Create sample permissions: manage users, manage attendances, view reports, request leave
    - Assign relevant permissions to each role
    - Prepare roles and permissions for application authorization

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,18 +4,44 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
         $roles = ['Admin', 'HRD', 'Atasan', 'Karyawan'];
 
-        foreach ($roles as $role) {
-            Role::firstOrCreate(['name' => $role]);
+        $permissions = [
+            'manage users',
+            'manage attendances',
+            'view reports',
+            'request leave',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        foreach ($roles as $roleName) {
+            $role = Role::firstOrCreate(['name' => $roleName]);
+
+            switch ($roleName) {
+                case 'Admin':
+                    $role->syncPermissions($permissions);
+                    break;
+                case 'HRD':
+                    $role->syncPermissions(['manage attendances', 'view reports']);
+                    break;
+                case 'Atasan':
+                    $role->syncPermissions(['view reports']);
+                    break;
+                case 'Karyawan':
+                    $role->syncPermissions(['request leave']);
+                    break;
+            }
         }
     }
 }
